683b3c4e15cf1d2936e6ef16f9c42f6e5e3cca47
poornima128 <149149026+poornima128@users.noreply.github.com>
2025-07-16 12:48:58 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,31 @@
+/*
+function greet (a){
+    return"hello, My dear "+a;
+
+}
+let op=greet("Poorni,God Bless You")
+console.log(op);*/
+/*
+const greet=(i)=>{
+    return"Hello,students,this is "+i;
+
+};
+let op=greet("VishnuPriya")
+console.log(op);*/
+
+/*const add =(a,b)=>{
+    return a+b;
+};
+console.log(add(3,6));*/
+/*const mul =(b,c)=>{
+    return b*c;
+
+};
+console.log(mul(3,2));*/
+/*const sqr=b=>{
+    return b*b;
+};
+console.log(sqr(5));*/
+const addition=(x,y)=> x+y;
+console.log(addition(2,4));
+
